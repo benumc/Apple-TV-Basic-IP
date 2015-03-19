c13ac974ad70262d3d120aca2d2d623a441b0466
benumc <ben@paramountautomation.com>
2015-03-18 17:17:38 -0700

    Whoops

--- AppleTVpairing.rb
+++ AppleTVpairing.rb
@@ -11,39 +11,6 @@ def ReadFromRemote(sock)
   return data
 end
 
-def ServerGET(ip,url)
-  uri = URI.parse("http://#{ip}:3689#{url}")
-  http = Net::HTTP.new(uri.host, uri.port)
-  http.read_timeout = 500
-  req = Net::HTTP::Get.new(uri.request_uri)
-  req['Host']= "192.168.1.20:3689"
-  req['User-Agent'] = "iPod"
-  req['Accept'] = "text/html,application/xhtml+xml,application/xml"
-  req['Accept-Language'] = "en-us"
-  req['Connection'] = "keep-alive"
-  req['Viewer-Only-Client'] = 1
-  req['Client-Daap-Version'] = 3.10
-  r = http.request(req)
-  return r
-end
-
-def ServerPOST(ip,url,data)
-  uri = URI.parse("http://#{ip}:3689#{url}")
-  req = Net::HTTP::Post.new(uri)
-  req['Host']= "192.168.1.20:3689"
-  req['User-Agent'] = "iPod"
-  req['Accept'] = "text/html,application/xhtml+xml,application/xml"
-  req['Accept-Language'] = "en-us"
-  req['Connection'] = "keep-alive"
-  req['Viewer-Only-Client'] = 1
-  req['Client-Daap-Version'] = 3.10
-  req.body = data
-  r = Net::HTTP.start(uri.hostname, uri.port) do |http|
-    http.request(req)
-  end
-  return r  
-end
-
 def AppleTVReply(sock)
   r = "cmpg\x00\x00\x00\x08\x00\x00\x00\x00\x00\x00\x00\x01"+
     "cmnm\x00\x00\x00\x06Savant"+
@@ -75,16 +42,5 @@ def AppleTVpair()
   return true
 end
 
-def AppleTVlogin(ip)
-  begin
-    r = ServerGET(ip,"/login?pairing-guid=0x0000000000000001").body
-    /mlid....(....)/.match(r)
-    sid = $1.unpack('H*')[0].to_i(16)
-    return sid
-  rescue
-    AppleTVpair()
-    retry
-  end
-end
 
 AppleTVpair()
